e0699e7ee705afd8d14bbd2c50d9412121a8a82f
Andrey Shabalin <shabalinworkpost@gmail.com>
2024-04-08 18:59:02 +0300

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/qqmIlToNpp/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placeList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(addCard, deleteCard) {
+  const cardBox = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardBox.querySelector(".card__image");
+  const cardTitle = cardBox.querySelector(".card__title");
+  cardImage.src = addCard.link;
+  cardImage.alt = addCard.name;
+  cardTitle.textContent = addCard.name;
+  const cardDeleteButton = cardBox.querySelector(".card__delete-button");
+  cardDeleteButton.addEventListener("click", function () {
+    deleteCard(cardBox);
+  });
+  return cardBox;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardBox) {
+  cardBox.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (cardBox) {
+  placeList.append(createCard(cardBox, deleteCard));
+});
